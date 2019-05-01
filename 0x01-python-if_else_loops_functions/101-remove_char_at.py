742a6a416956f99f7325fa46180684fc2932af29
billhong6981 <bill.burton6981@gmail.com>
2019-05-01 08:12:42 +0000

    spaces

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
     else:
-        str = str[:n] + str[n + 1 : len(str)]
+        str = str[:n] + str[n + 1:len(str)]
         return str
