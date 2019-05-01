650979ce3128b0f076292b8bea2fd46b4d7a3322
billhong6981 <bill.burton6981@gmail.com>
2019-05-01 04:10:49 +0000

    spaces

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,6 @@ def uppercase(str):
         if val >= 97 and val <= 123:
             val = val - 32
         if i != len(str) - 1:
-            print('{:c}'.format(val), end = "")
+            print('{:c}'.format(val), end="")
     else:
         print('{:c}'.format(val))
