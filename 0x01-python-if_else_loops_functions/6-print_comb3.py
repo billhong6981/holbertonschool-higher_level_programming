14b212657d8fe1cf968a64bd64590b30186a8cfe
billhong6981 <bill.burton6981@gmail.com>
2019-05-01 04:14:16 +0000

    white spaces

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print('{}{}'.format(i, j), end = ", ")
+            print('{:d}{:d}'.format(i, j), end=", ")
 else:
-    print('{}{}'.format(i,j))
+    print('{:d}{:d}'.format(i, j))
