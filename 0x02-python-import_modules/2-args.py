106749163d72512edbd22ae638a4e85878b5270b
billhong6981 <bill.burton6981@gmail.com>
2019-05-03 02:00:02 +0000

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) == 1:
-        print('0 arguments.')
-        sys.exit()
     if len(sys.argv) == 2:
         print('1 argument:')
     else:
-        print('{:d} arguments:'.format(len(sys.argv) - 1))
-        for i, c in list(enumerate(sys.argv, start=0)):
-            if i != 0:
-                print('{:d}: {}'.format(i, c))
+        if len(sys.argv) == 1:
+            print('0 arguments.')
+        else:
+            print('{:d} arguments:'.format(len(sys.argv) - 1))
+    for i, c in list(enumerate(sys.argv, start=0)):
+        if i != 0:
+            print('{:d}: {}'.format(i, c))
