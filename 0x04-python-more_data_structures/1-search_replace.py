a05b5d58a792129811a14f30673777f6a0025991
billhong6981 <bill.burton6981@gmail.com>
2019-05-10 05:00:12 +0000

    task1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/python3
 def search_replace(my_list, search, replace):
-    n_list = my_list.copy()
-
-    idx = n_list.index(search)
-    n_list.remove(search)
-    n_list.insert(idx, replace)
-
+    n_list = []
+    n_list = list(map(lambda x: replace if x is search else x, my_list))
     return (n_list)
