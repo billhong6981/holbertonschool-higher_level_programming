fc6eeb260e2f96a576f748db8f4fbf51aff8e7f0
billhong6981 <bill.burton6981@gmail.com>
2019-05-10 05:42:28 +0000

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,17 +14,14 @@ def roman_to_int(roman_string):
     sum = 0
     if type(roman_string) is not str or roman_string is None:
             return (sum)
-
     # set up a roman number dictionary
     dict_roman = dict(M=1000, Z=900, D=500, T=400, C=100, G=90, L=50, Q=
-40, X=10, P=9, V=5, R=4, I=1)
+                      40, X=10, P=9, V=5, R=4, I=1)
     list_a = ["CM", "CD", "XC", "IL", "IX", "IV"]
     list_b = ['Z', 'T', 'G', 'Q', 'P', 'R']
-
     for k, v in zip(list_a, list_b):
         if k in roman_string:
             roman_string = replace_str(roman_string, k, v)
-
     for i in range(len(roman_string)):
-        sum += (dict_roman[roman_string[i]])
+        sum += dict_roman[roman_string[i]]
     return (sum)
