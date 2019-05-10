a1a99eb1f4d40c952a7fef3f750c4519f890eab9
billhong6981 <bill.burton6981@gmail.com>
2019-05-10 05:37:18 +0000

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
     def replace_str(str=None, str_find=None, str_replace=None):
         idx = str.find(str_find)
         if idx == 0:
@@ -18,7 +17,7 @@ def roman_to_int(roman_string):
 
     # set up a roman number dictionary
     dict_roman = dict(M=1000, Z=900, D=500, T=400, C=100, G=90, L=50, Q=
-                      40, X=10, P=9, V=5, R=4, I=1)
+40, X=10, P=9, V=5, R=4, I=1)
     list_a = ["CM", "CD", "XC", "IL", "IX", "IV"]
     list_b = ['Z', 'T', 'G', 'Q', 'P', 'R']
 
