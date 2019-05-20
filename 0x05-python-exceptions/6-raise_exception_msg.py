f2074d62160aa58e0eed16b4cf39b052cf657463
billhong6981 <bill.burton6981@gmail.com>
2019-05-20 23:53:39 +0000

    fix checker

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyton3
 def raise_exception_msg(message=""):
     try:
-        raise NameError(message)
+        raise NameError
     except:
-        pass
+        raise NameError(message)
