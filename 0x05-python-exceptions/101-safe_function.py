2b53c13c81722df07cd0dcd340e13beb4ca67f22
billhong6981 <bill.burton6981@gmail.com>
2019-05-21 01:47:55 +0000

    advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        fct(*args)
+        return (fct(*args))
+    except ZeroDivisionError as err:
+        sys.stderr.write('Exception: ' + str(err) + '\n')
+        return (None)
+    except IndexError as err:
+        sys.stderr.write('Exception: ' + str(err) + '\n')
+        return (None)
