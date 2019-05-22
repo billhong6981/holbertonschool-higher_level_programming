843f6f0a951c3fc56b8722d5be46f59a32a2c85d
billhong6981 <bill.burton6981@gmail.com>
2019-05-22 21:30:03 +0000

    advanced task100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
 class Node:
+    """Creates class named Node
+
+    """
 
     def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next_node = next_node
+        """initializes instance
+
+        Args:
+            data: integer
+            next_node: next node address
+
+        """
+
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
+        """Decorates getter function to access private instance attribute
+
+        Return:
+            return self.__data value
+
+        """
+
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Decorates setter function to set private instance attribute value
+
+        Raises:
+            TypeError: data must be an integer
+
+        """
+
         if type(value) is not int:
             raise TypeError('data must be an integer')
         self.__data = value
@@ -27,14 +52,28 @@ class Node:
 
 
 class SinglyLinkedList:
+    """Creates class
+
+    """
 
     def __init__(self):
         self.head = None
 
     def __str__(self):
+        """Return string to print function
+
+        """
+
         return self.print_list()
 
     def sorted_insert(self, value):
+        """Insert node function
+
+        Arg:
+            value: integer
+
+        """
+
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -59,6 +98,13 @@ class SinglyLinkedList:
     """
 
     def print_list(self):
+        """makes string for print function
+
+        Return:
+            return string
+
+        """
+
         string = ''
         current = self.head
         while current.next_node is not None:
