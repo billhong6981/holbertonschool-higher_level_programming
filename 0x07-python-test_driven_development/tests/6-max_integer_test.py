b4574d46838c78043c09be14d7c868d4c40fdb8f
billhong6981 <bill.burton6981@gmail.com>
2019-05-26 20:06:53 +0000

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,31 +1,50 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+"""Unittest for max_integer([..])"""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Test Max Integer Class"""
 
     def test_max_integer(self):
-      A = [1, 2, 3, 4]
-      self.assertEqual(max_integer(A), 4)
+        "Normal test"""
+        A = [1, 2, 3, 4]
+        self.assertEqual(max_integer(A), 4)
 
-    def test_float(self):
-      B = [3, 4, 6, 7.1]
-      self.assertEqual(max_integer(B), 7.1)
+    def test_at_begin(self):
+        """max at biginning"""
+        B = [8, 4, 6]
+        self.assertEqual(max_integer(B), 8)
+
+    def test_at_middle(self):
+        """max at middle"""
+        self.assertEqual(max_integer([2, 5, 3]), 5)
+
+    def test_at_end(self):
+        """max at the end"""
+        self.assertEqual(max_integer([3, 5, 7]), 7)
+
+    def test_one_negative(self):
+        """Test for “one negative number in the list” exists"""
+        self.assertEqual(max_integer([-3]), -3)
+
+    def test_only_negative(self):
+        """Test for only negative number in list"""
+        self.assertEqual(max_integer([-1, -5, -2]), -1)
 
     def test_empty(self):
-      A = []
-      self.assertEqual(max_integer(A), None)
+        A = []
+        self.assertEqual(max_integer(A), None)
 
     def test_one_element(self):
-      A = [1]
-      self.assertEqual(max_integer(A), 1)
+        A = [1]
+        self.assertEqual(max_integer(A), 1)
 
     def test_string(self):
-      A = [1, "hello", 3]
-      with self.assertRaises(TypeError):
-          max_integer(A)
+        A = [1, "hello", 3]
+        with self.assertRaises(TypeError):
+            max_integer(A)
 
 if __name__ == '__main__':
     unittest.main()
