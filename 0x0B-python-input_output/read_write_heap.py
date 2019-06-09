52936a71856684f10dc70e9e407266b0488f1fbe
billhong6981 <bill.burton6981@gmail.com>
2019-06-09 09:06:10 +0000

    fix len

--- 0x0B-python-input_output/read_write_heap.py
+++ 0x0B-python-input_output/read_write_heap.py
@@ -77,6 +77,9 @@ if __name__ == "__main__":
 
         # write the new string
         mem_file.seek(addr_start + i)
+        if len(search_string) > len(replace_string):
+            n = len(search_string) - len(replace_string)
+            replace_string = replace_string + " " * n
         mem_file.write(bytes(replace_string, "ASCII"))
         print("replace: [{}]\n".format(replace_string))
         mem_file.close()
