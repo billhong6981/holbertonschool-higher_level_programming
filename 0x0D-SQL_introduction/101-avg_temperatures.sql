92e3a27fde020150918fc63aab940e845ca510b4
billhong6981 <bill.burton6981@gmail.com>
2019-07-16 05:21:00 +0000

    spelling mistake

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,6 @@
 /* 101-avg_temperatures.sql
    displays the average temperature (Fahrenheit)
    by city ordered by temperature (descending). */
-SELECT city, AVG(value) AS avg_temp FROM temperature ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+       GROUP BY city
+       ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 /* 103-max_state.sql
    displays the max temperature of each state (ordered by State name). */
-SELECT state, MAX(value) AS max_temp FROM temperature
-       ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures
+       GROUP BY state ORDER BY state;
