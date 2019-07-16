ff7320196ddf795fbd2fcc1b5f22e91aaf1b615a
billhong6981 <bill.burton6981@gmail.com>
2019-07-16 05:44:30 +0000

    comment space required

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,10 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
---import DATABASE
+-- import DATABASE
 USE hbtn_0c_0;
---converts field to utf-8 format
+-- converts field to utf-8 format
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
---converts table to utf-8 format
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---converts database to utf-8 format
+-- converts table to utf-8 format
+ALTER TABLE first_table CONVERT TO
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- converts database to utf-8 format
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
