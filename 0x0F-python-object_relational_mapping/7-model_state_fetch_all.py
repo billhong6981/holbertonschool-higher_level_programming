dc66e3f2d68fb54c9a97e6d75327a29f06f5e004
billhong6981 <bill.burton6981@gmail.com>
2019-08-19 06:20:45 +0000

    add print space

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,13 +8,14 @@ from sqlalchemy import create_engine
 
 def list_state():
     """list my states"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
+    port = 3306
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                           .format(argv[1], argv[2], port, argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
-        print("{}:{}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
 
 if __name__ == "__main__":
