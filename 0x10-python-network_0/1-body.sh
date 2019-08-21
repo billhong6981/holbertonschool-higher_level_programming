022de10d22f52a40cbe23d0c45a1b33f7b8cac88
billhong6981 <bill.burton6981@gmail.com>
2019-08-21 02:57:38 +0000

    add slash

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 # displays the body of the response
-if [ "$(curl -swL "%{http_code}" $1 -o /dev/null)" -eq "200" ]; then curl -sL "$1"; fi
+if [ "$(curl -swL \"%{http_code}\" $1 -o /dev/null)" -eq 200 ]; then curl -sL "$1"; fi
