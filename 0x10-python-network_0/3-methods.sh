45275aff48285972261ffd87c992653debe9e527
billhong6981 <bill.burton6981@gmail.com>
2019-08-21 02:53:14 +0000

    fix body status code

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-curl -sIX OPTIONS "$1" | grep Allow: | sed 's/Allow: //'
-
+curl -sIX OPTIONS "$1" | grep -e "Allow:" | sed 's/Allow: //'
